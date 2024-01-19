9e68a91acaedc12226cd7de66383398cd7834009
htripathi12 <tripathh@purdue.edu>
2024-01-19 18:04:52 -0500

    format changes, TODO: make slide animation for login/signup

--- src/Login.js
+++ src/Login.js
@@ -8,19 +8,21 @@ function Login() {
         <>
         <Button marginTop="3" bg='teal.400' border="1px" display="flex" flexDirection="row" color="white" _hover={{ bg: "teal.700", color: "white" }}>
             <Link as={RouterLink} to="/" _hover={{ textDecoration: "none" }}>
-            <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-arrow-big-left-filled" width="27" height="27" viewBox="0 0 24 24" stroke-width="1.5" stroke="#ffffff" fill="none" stroke-linecap="round" stroke-linejoin="round">
-                <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
-                <path d="M9.586 4l-6.586 6.586a2 2 0 0 0 0 2.828l6.586 6.586a2 2 0 0 0 2.18 .434l.145 -.068a2 2 0 0 0 1.089 -1.78v-2.586h7a2 2 0 0 0 2 -2v-4l-.005 -.15a2 2 0 0 0 -1.995 -1.85l-7 -.001v-2.585a2 2 0 0 0 -3.414 -1.414z" stroke-width="0" fill="currentColor" />
-            </svg>
+                <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-arrow-big-left-filled" 
+                    width="27" height="27" viewBox="0 0 24 24" stroke-width="1.5" stroke="#ffffff" fill="none" 
+                    stroke-linecap="round" stroke-linejoin="round">
+                    <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
+                    <path d="M9.586 4l-6.586 6.586a2 2 0 0 0 0 2.828l6.586 6.586a2 2 0 0 0 2.18 .434l.145 -.068a2 2 0 0 0 1.089 -1.78v-2.586h7a2 2 0 0 0 2 -2v-4l-.005 -.15a2 2 0 0 0 -1.995 -1.85l-7 -.001v-2.585a2 2 0 0 0 -3.414 -1.414z" stroke-width="0" fill="currentColor" />
+                </svg>
             </Link>
             <Link as={RouterLink} to="/" _hover={{ textDecoration: "none" }}><Text ml={2}>Back</Text></Link>
         </Button>
         <AbsoluteCenter>
-            <Container bg='teal.600' borderRadius="30" w='500px' h='400px' color="white" p={5} shadow="2xl"border="2px" borderColor="gray.400">            
+            <Container bg='teal.600' borderRadius="30" w='500px' h='400px' color="white" p={5} shadow="2xl" border="2px" borderColor="gray.400">            
                 <Tabs isFitted variant="soft-rounded">
-                    <TabList mb="1em">
-                        <Tab color="white" _hover={{ color: "white", bg: "teal.800"}}>Login</Tab>
-                        <Tab color="white" _hover={{ color: "white", bg: "teal.800" }}>Sign Up</Tab>
+                    <TabList mb="1em" border="1px" borderRadius="21px">
+                        <Tab color="white" _hover={{ color: "white", bg: "teal.800"}} _active={{ transform: "translateX(10px)", transition: "transform 0.2s" }}>Login</Tab>
+                        <Tab color="white" _hover={{ color: "white", bg: "teal.800"}} _active={{ transform: "translateX(10px)", transition: "transform 0.2s" }}>Sign Up</Tab>
                     </TabList>
                     <TabPanels>
                         <TabPanel>
@@ -63,5 +65,6 @@ function Login() {
         </AbsoluteCenter></>
     );
 }
+
 
 export default Login;
