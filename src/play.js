ee00b681f653ee99f1e7fb555ee129b2c4bce97f
htripathi12 <tripathh@purdue.edu>
2024-01-19 18:26:33 -0500

    added chessboard

--- src/play.js
+++ src/play.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import { Box, Center, Container, AbsoluteCenter, Flex, Link, Spacer, Button, ChakraProvider } from '@chakra-ui/react';
 import { BrowserRouter as Router, Link as RouterLink } from 'react-router-dom';
+import Chessboard from 'chessboardjsx';
 
 function Play() {
     return (
-        <div>Hello World</div>
+        <div id="board1" style={{width: '400px'}}>
+            <Chessboard position="start" draggable="true" />
+        </div>
     );
 }
+
 
 export default Play;
