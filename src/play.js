0b95750a2aeec8cc8828e05ddc0006adf07681a9
htripathi12 <tripathh@purdue.edu>
2024-01-20 13:57:49 -0500

    chess board kinda works, highlights last move but doesn't handle invalid ones

--- src/play.js
+++ src/play.js
@@ -1,15 +1,49 @@
-import React from 'react';
+import React, { useRef, useState, useEffect } from 'react';
 import { Box, Center, Container, AbsoluteCenter, Flex, Link, Spacer, Button, ChakraProvider } from '@chakra-ui/react';
 import { BrowserRouter as Router, Link as RouterLink } from 'react-router-dom';
+
 import Chessboard from 'chessboardjsx';
+import { Chess } from 'chess.js';
 
 function Play() {
+    const chess = useRef(new Chess());
+    const [fen, setFen] = useState('start');
+    const [squareStyles, setSquareStyles] = useState({});
+
+    useEffect(() => {
+        setFen(chess.current.fen());
+    }, [chess]);
+
+    const onDrop = ({ sourceSquare, targetSquare }) => {
+        let move = chess.current.move({
+            from: sourceSquare,
+            to: targetSquare,
+            promotion: 'q'
+        });
+
+        if (move === null) {
+            alert("Invalid move");
+            return;
+        }
+
+        setSquareStyles({
+            [sourceSquare]: { backgroundColor: 'rgba(255, 255, 0, 0.4)' },
+            [targetSquare]: { backgroundColor: 'rgba(255, 255, 0, 0.4)' }
+        });
+
+        setFen(chess.current.fen());
+    };
+
     return (
         <div id="board1" style={{width: '400px'}}>
-            <Chessboard position="start" draggable="true" />
+            <Chessboard 
+                position={fen}
+                onDrop={onDrop}
+                draggable={true}
+                squareStyles={squareStyles}
+            />
         </div>
     );
 }
-
 
 export default Play;
