6d3152ed424bc1d2cd1a7715d1e9398f32588713
htripathi12 <tripathh@purdue.edu>
2024-05-09 00:10:28 -0400

    back button

--- src/Learn.js
+++ src/Learn.js
@@ -1,5 +1,8 @@
 import React from 'react';
 import StudyBlock from './StudyBlock';
+import { BrowserRouter as Router, Link as RouterLink } from 'react-router-dom';
+import { Box, Center, Container, AbsoluteCenter, Flex, Link, Spacer, Button, Image, ChakraProvider, FormControl, FormLabel, 
+    Input, Stack, Text, Tabs, TabList, TabPanels, Tab, TabPanel } from '@chakra-ui/react';
 
 import CaroKann from './Caro-Kann.png';
 import ModernDefense from './ModernDefense.png';
@@ -7,30 +10,43 @@ import SicilianDefense from './SicilianDefense.png';
 
 function Learn() {
     return (
-        <>
-        <div style={{display: 'flex', flexDirection: 'row', marginTop: '25px', justifyContent: 'space-evenly'}}>            
-            <StudyBlock to="/caro-kann" imageUrl={CaroKann} title="Caro-Kann Defense">
-                Dive into the intriguing world of the Caro-Kann Defense, where Black meets 1. e4 with the confident
-                stride of 1...c6. This defense boasts a robust structure that allows for swift piece development, all
-                while laying the groundwork for a resilient defense. With careful play, the Caro-Kann promises a
-                fortress-like defense fit for any strategic mastermind.
-            </StudyBlock>
-            
-            <StudyBlock to="/modern-defense" imageUrl={ModernDefense} title="Modern Defense">
-                Explore the Modern Defense (1...g6), where Black delays central pawn advances for flexible piece 
-                deployment. The fianchettoed bishop on g7 pressures the long diagonal, creating asymmetry and 
-                counterattacking opportunities. Despite sacrificing immediate central control, the Modern Defense's 
-                strategic flexibility makes it a captivating choice for disrupting traditional chess paradigms.
-            </StudyBlock>
+        <div>
+            <Button marginTop="3" bg='teal.400' border="1px" display="flex" flexDirection="row" color="white" _hover={{ bg: "teal.700", color: "white" }}>
+                <Link as={RouterLink} to="/" _hover={{ textDecoration: "none" }}>
+                    <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-arrow-big-left-filled" 
+                        width="27" height="27" viewBox="0 0 24 24" stroke-width="1.5" stroke="#ffffff" fill="none" 
+                        stroke-linecap="round" stroke-linejoin="round">
+                        <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
+                        <path d="M9.586 4l-6.586 6.586a2 2 0 0 0 0 2.828l6.586 6.586a2 2 0 0 0 2.18 .434l.145 -.068a2 2 0 0 
+                            0 1.089 -1.78v-2.586h7a2 2 0 0 0 2 -2v-4l-.005 -.15a2 2 0 0 0 -1.995 -1.85l-7 -.001v-2.585a2 2 0
+                            0 0 -3.414 -1.414z" stroke-width="0" fill="currentColor" />
+                    </svg>
+                </Link>
+                <Link as={RouterLink} to="/" _hover={{ textDecoration: "none" }}><Text ml={2}>Back</Text></Link>
+            </Button>
+            <div style={{display: 'flex', flexDirection: 'row', marginTop: '30px', justifyContent: 'space-evenly'}}>            
+                <StudyBlock to="/caro-kann" imageUrl={CaroKann} title="Caro-Kann Defense">
+                    Dive into the intriguing world of the Caro-Kann Defense, where Black meets 1. e4 with the confident
+                    stride of 1...c6. This defense boasts a robust structure that allows for swift piece development, all
+                    while laying the groundwork for a resilient defense. With careful play, the Caro-Kann promises a
+                    fortress-like defense fit for any strategic mastermind.
+                </StudyBlock>
+                
+                <StudyBlock to="/modern-defense" imageUrl={ModernDefense} title="Modern Defense">
+                    Explore the Modern Defense (1...g6), where Black delays central pawn advances for flexible piece 
+                    deployment. The fianchettoed bishop on g7 pressures the long diagonal, creating asymmetry and 
+                    counterattacking opportunities. Despite sacrificing immediate central control, the Modern Defense's 
+                    strategic flexibility makes it a captivating choice for disrupting traditional chess paradigms.
+                </StudyBlock>
 
-            <StudyBlock to="/sicilian-defense" imageUrl={SicilianDefense} title="Sicilian Defense">
-                Discover the Sicilian Defense, where Black's counterattacking move 1...c5 targets White's central pawn 
-                structure. With its rich history and numerous variations, the Sicilian offers a wide range of strategic 
-                possibilities. Despite conceding the center early on, the Sicilian Defense rewards boldness and 
-                resourcefulness, leading to thrilling battles for the center.
-            </StudyBlock>
+                <StudyBlock to="/sicilian-defense" imageUrl={SicilianDefense} title="Sicilian Defense">
+                    Discover the Sicilian Defense, where Black's counterattacking move 1...c5 targets White's central pawn 
+                    structure. With its rich history and numerous variations, the Sicilian offers a wide range of strategic 
+                    possibilities. Despite conceding the center early on, the Sicilian Defense rewards boldness and 
+                    resourcefulness, leading to thrilling battles for the center.
+                </StudyBlock>
+            </div>
         </div>
-        </>
     );
 }
 export default Learn;
