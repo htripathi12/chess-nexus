ce3f05759931caa91d0693686982ed601394ec3c
htripathi12 <tripathh@purdue.edu>
2024-06-06 21:04:46 -0400

    tab route handling fixed, TODO: login post handling

--- src/database.js
+++ src/database.js
@@ -21,52 +21,23 @@ db.connect((err) => {
     console.log('Connected to the database successfully');
 });
 
-router.post('/login', (req, res) => {
+router.post('/signup', (req, res) => {
     const { email, password, regState } = req.body;
 
     console.log(`Received login/signup request with email: ${email} and state: ${regState}`);
-
-    if (regState === 'signup') {
-        bcrypt.hash(password, saltRounds, (err, hash) => {
+    bcrypt.hash(password, saltRounds, (err, hash) => {
+        if (err) {
+            console.error('Error hashing password:', err);
+            return res.status(500).send('Error processing request');
+        }
+        db.query('INSERT INTO users (email, password) VALUES (?, ?)', [email, hash], (err, result) => {
             if (err) {
-                console.error('Error hashing password:', err);
-                return res.status(500).send('Error processing request');
+                console.error('Error inserting user:', err);
+                return res.status(500).send('Error inserting values');
             }
-            db.query('INSERT INTO users (email, password) VALUES (?, ?)', [email, hash], (err, result) => {
-                if (err) {
-                    console.error('Error inserting user:', err);
-                    return res.status(500).send('Error inserting values');
-                }
-                res.send('Values Inserted');
-            });
+            res.send('Values Inserted');
         });
-    } 
-    else if (regState === 'login') {
-        db.query('SELECT password FROM users WHERE email = ?', [email], (err, result) => {
-            if (err) {
-                console.error('Error querying the database:', err);
-                return res.status(500).send('Error processing request');
-            }
-
-            console.log("Result: ", result);
-
-            if (result.length > 0) {
-                bcrypt.compare(password, result[0].password, (err, isMatch) => {
-                    if (err) {
-                        console.error('Error comparing passwords:', err);
-                        return res.status(500).send('Error processing request');
-                    }
-                    isMatch ? res.send('Login successful') : res.status(401).send('Invalid password');
-                    console.log('Login: ', isMatch);
-                });
-            } else {
-                res.status(404).send('User not found');
-            }
-        });
-    } 
-    else {
-        res.status(400).send('Invalid registration state');
-    }
+    });
 });
 
 module.exports = router;
