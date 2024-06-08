a4529b8985fec9e60c8c0888919b99b3062e72d3
htripathi12 <tripathh@purdue.edu>
2024-06-07 20:26:51 -0400

    rest of files

--- src/Puzzles/randomPuzzle.js
+++ src/Puzzles/randomPuzzle.js
@@ -2,51 +2,9 @@ const fs = require('fs');
 const express = require('express');
 const router = express.Router();
 
-function loadAndSelectPuzzle(filename, callback) {
-    fs.readFile(filename, 'utf8', (err, data) => {
-        if (err) {
-            console.error(`Error reading file: ${err}`);
-            return;
-        }
-
-        const lines = data.split('\n');
-        const puzzles = [];
-        let puzzle = {};
-
-        for (const line of lines) {
-            if (line.startsWith('FEN')) {
-                if (Object.keys(puzzle).length !== 0) {
-                    puzzles.push(puzzle);
-                    puzzle = {};
-                }
-                puzzle['FEN'] = line.split(':')[1].trim();
-            } else if (line.startsWith('WhiteElo')) {
-                puzzle['WhiteElo'] = line.split(':')[1].trim();
-            } else if (line.startsWith('Move Sequence')) {
-                puzzle['Move Sequence'] = line.split(':')[1].trim();
-            }
-        }
-
-        if (Object.keys(puzzle).length !== 0) {
-            puzzles.push(puzzle);
-        }
-
-        const randomPuzzle = puzzles[Math.floor(Math.random() * puzzles.length)];
-
-        callback(randomPuzzle);
-    });
-}
-
-router.get('/', (req, res) => { // Change the route to '/'
-    const filename = 'src/Puzzles/formatted_pgn_output.txt';
+router.get('/', (req, res) => {
     console.log('GET /puzzles');
-    loadAndSelectPuzzle(filename, (randomPuzzle) => {
-        // Extract the FEN and move sequence from the random puzzle
-        const { FEN, 'Move Sequence': moveSequence } = randomPuzzle;
-
-        // Send the FEN and move sequence as a JSON response
-        res.json({ FEN, moveSequence });
-    });
+    res.send("Hello from puzzles!");
 });
 
 module.exports = router;
--- src/pages/Puzzles.js
+++ src/pages/Puzzles.js
@@ -6,9 +6,11 @@ import { Box, Center, Container, AbsoluteCenter, Flex, Link, Spacer, Button, Ima
 
 function Puzzles() {
     const [fen, setFen] = useState('');
-    const [moveSequence, setMoveSequence] = useState(''); // Add a new state for the move sequence
+    const [winLoss, setWinLoss] = useState('');
+    const [history, setHistory] = useState([]);
     const customBoardRef = useRef(null);
 
+    
     useEffect(() => {
         // Disable scrolling when the component is mounted
         document.body.style.overflow = 'hidden';
@@ -21,8 +23,7 @@ function Puzzles() {
     const getNextPuzzle = () => {
         Axios.get('http://localhost:3000/puzzles')
             .then((response) => {
-                setFen(response.data.FEN); // Update the fen state
-                setMoveSequence(response.data.moveSequence); // Update the move sequence state
+                console.log(response);
             })
             .catch((error) => {
                 console.error('There was an error!', error);
@@ -32,11 +33,12 @@ function Puzzles() {
     return (
         <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100vh', paddingBottom: '50px' }}>
             <div style={{ margin: 'auto', display: 'flex', flexDirection: 'row'}}>
-                <CustomBoard
-                    ref={customBoardRef}
-                    fen={fen}
-                    setFen={setFen}
-                />
+                    <CustomBoard
+                        ref={customBoardRef}
+                        fen={fen}
+                        setFen={setFen}
+                        setWinLoss={setWinLoss}
+                    />
                 <Button onClick={getNextPuzzle}>Next Puzzle</Button>
             </div>
         </div>
