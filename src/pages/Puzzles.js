b59e40fc72397257f22504a4901c33d6a93a9202
htripathi12 <tripathh@purdue.edu>
2024-06-13 18:23:24 -0400

    changed button location and styling

--- src/pages/Puzzles.js
+++ src/pages/Puzzles.js
@@ -46,15 +46,20 @@ function Puzzles() {
 
     return (
         <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100vh', paddingBottom: '50px' }}>
-            <div style={{ margin: 'auto', display: 'flex', flexDirection: 'row' }}>
-                <CustomBoard
-                    ref={customBoardRef}
-                    fen={fen}
-                    orientation={orientation} // Pass the orientation prop
-                    setFen={setFen}
-                />
-                <Button onClick={getNextPuzzle}>Next Puzzle</Button>
-            </div>
+            <div style={{ margin: 'auto', display: 'flex', flexDirection: 'column', alignItems: 'flex-end' }}>
+    <CustomBoard
+        ref={customBoardRef}
+        fen={fen}
+        orientation={orientation} // Pass the orientation prop
+        setFen={setFen}
+    />
+    <Button onClick={getNextPuzzle} marginTop="3" bg='teal.400' border="1px" display="flex" flexDirection="row" color="white" _hover={{ bg: "teal.700", color: "white" }}>
+        <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-arrow-big-right-filled" width="35" height="35" viewBox="0 0 24 24" stroke-width="1.5" stroke="#ffffff" fill="none" stroke-linecap="round" stroke-linejoin="round">
+            <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
+            <path d="M12.089 3.634a2 2 0 0 0 -1.089 1.78l-.001 2.586h-6.999a2 2 0 0 0 -2 2v4l.005 .15a2 2 0 0 0 1.995 1.85l6.999 -.001l.001 2.587a2 2 0 0 0 3.414 1.414l6.586 -6.586a2 2 0 0 0 0 -2.828l-6.586 -6.586a2 2 0 0 0 -2.18 -.434l-.145 .068z" stroke-width="0" fill="currentColor" />
+        </svg>
+    </Button>
+</div>
         </div>
     );
 }
