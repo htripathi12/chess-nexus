e0fa2f916c9d33aec04310771e4e3ce446c24610
htripathi12 <tripathh@purdue.edu>
2024-06-13 17:36:29 -0400

    play first move

--- src/pages/Puzzles.js
+++ src/pages/Puzzles.js
@@ -2,12 +2,13 @@ import React, { useState, useRef, useEffect } from 'react';
 import CustomBoard from '../components/CustomBoard';
 import Axios from 'axios';
 import { Button } from '@chakra-ui/react';
+import { Chess } from 'chess.js';
 
 function Puzzles() {
     const [fen, setFen] = useState('rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1');
     const [orientation, setOrientation] = useState('white');
-    const [winLoss, setWinLoss] = useState('');
     const customBoardRef = useRef(null);
+    const chess = useRef(new Chess());
 
     useEffect(() => {
         document.body.style.overflow = 'hidden';
@@ -19,10 +20,25 @@ function Puzzles() {
     const getNextPuzzle = async () => {
         try {
             const response = await Axios.get('http://localhost:3000/puzzles');
-            setFen(response.data.fen);
-            const activePlayer = response.data.fen.split(' ')[1];
+            const newFen = response.data.fen;
+            const firstMove = response.data.moves.split(' ')[0];
+
+            chess.current.load(newFen);
+
+            // Set the new FEN and orientation
+            setFen(newFen);
+            const activePlayer = newFen.split(' ')[1];
             setOrientation(activePlayer === 'w' ? 'black' : 'white');
-            console.log(response.data);
+
+            // Make the first move in UCI format after a 1 second delay
+            setTimeout(() => {
+                chess.current.move(firstMove);
+
+                const updatedFen = chess.current.fen();
+                setFen(updatedFen);
+
+                console.log(response.data);
+            }, 1000);
         } catch (error) {
             console.error('There was an error!', error);
         }
@@ -36,7 +52,6 @@ function Puzzles() {
                     fen={fen}
                     orientation={orientation} // Pass the orientation prop
                     setFen={setFen}
-                    setWinLoss={setWinLoss}
                 />
                 <Button onClick={getNextPuzzle}>Next Puzzle</Button>
             </div>
