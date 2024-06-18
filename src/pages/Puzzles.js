f910adaf1c5ad2d94fbb43a74bf364917d36ca29
htripathi12 <tripathh@purdue.edu>
2024-06-18 18:31:27 -0400

    fixed conditional rendering

--- src/pages/Puzzles.js
+++ src/pages/Puzzles.js
@@ -57,8 +57,10 @@ function Puzzles() {
         if (userMove === expectedMove) {
             console.log('Correct');
             const nextMove = moves[moveIndex + 1];
+            setPuzzleCompleted(true);
             if (nextMove) {
                 setTimeout(() => {
+                    setPuzzleCompleted(false);
                     chess.current.move(nextMove);
                     setFen(chess.current.fen());
                     setMoveIndex(moveIndex => moveIndex + 2);
@@ -88,8 +90,8 @@ function Puzzles() {
     };
 
     return (
-        <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100vh', paddingBottom: '50px' }}>
-            <div style={{ margin: 'auto', display: 'flex', flexDirection: 'column', alignItems: 'flex-end' }}>
+        <div style={{ display: 'grid', gridTemplateColumns: 'auto auto', justifyContent: 'center', alignItems: 'center', height: '100vh', paddingBottom: '50px', position: 'relative' }}>
+            <div style={{ margin: 'auto', display: 'flex', flexDirection: 'column', alignItems: 'flex-end', position: 'relative' }}>
                 <CustomBoard
                     ref={customBoardRef}
                     fen={fen}
@@ -100,38 +102,39 @@ function Puzzles() {
                 />
                 <div style={{ display: 'flex', justifyContent: 'space-between', width: '100%', marginTop: '3', padding: '10px' }}>
                     <Button onClick={redoPuzzle} bg='teal.400' border="1px" color="white" _hover={{ bg: "teal.700", color: "white" }}>
-                        <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-reload" width="35" height="35" viewBox="0 0 24 24" stroke-width="1.5" stroke="#ffffff" fill="none" stroke-linecap="round" stroke-linejoin="round">
+                        <svg xmlns="http://www.w3.org/2000/svg" className="icon icon-tabler icon-tabler-reload" width="35" height="35" viewBox="0 0 24 24" stroke-width="1.5" stroke="#ffffff" fill="none" stroke-linecap="round" stroke-linejoin="round">
                             <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
                             <path d="M19.933 13.041a8 8 0 1 1 -9.925 -8.788c3.899 -1 7.935 1.007 9.425 4.747" />
                             <path d="M20 4v5h-5" />
                         </svg>
                     </Button>
                     <Button onClick={getNextPuzzle} bg='teal.400' border="1px" color="white" _hover={{ bg: "teal.700", color: "white" }}>
-                        <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-arrow-big-right-filled" width="35" height="35" viewBox="0 0 24 24" stroke-width="1.5" stroke="#ffffff" fill="none" stroke-linecap="round" stroke-linejoin="round">
+                        <svg xmlns="http://www.w3.org/2000/svg" className="icon icon-tabler icon-tabler-arrow-big-right-filled" width="35" height="35" viewBox="0 0 24 24" stroke-width="1.5" stroke="#ffffff" fill="none" stroke-linecap="round" stroke-linejoin="round">
                             <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
                             <path d="M12.089 3.634a2 2 0 0 0 -1.089 1.78l-.001 2.586h-6.999a2 2 0 0 0 -2 2v4l.005 .15a2 2 0 0 0 1.995 1.85l6.999 -.001l.001 2.587a2 2 0 0 0 3.414 1.414l6.586 -6.586a2 2 0 0 0 0 -2.828l-6.586 -6.586a2 2 0 0 0 -2.18 -.434l-.145 .068z" stroke-width="0" fill="currentColor" />
                         </svg>
                     </Button>
                 </div>
+                {puzzleCompleted &&
+                    <div style={{
+                        position: 'absolute',
+                        right: '-150px',
+                        top: '50%',
+                        transform: 'translateY(-50%)',
+                        padding: '20px',
+                        backgroundColor: '#282c34',
+                        borderRadius: '10px',
+                        color: '#fff',
+                        fontSize: '24px',
+                        fontWeight: 'bold',
+                        textAlign: 'center',
+                        boxShadow: '0px 0px 10px 3px rgba(0, 0, 0, 0.2)',
+                        border: '1px solid #fff'
+                    }}>Correct!</div>
+                }
             </div>
-            {puzzleCompleted &&
-                <div style={{
-                    position: 'absolute',
-                    top: '50%',
-                    left: '50%',
-                    transform: 'translate(-50%, -50%)',
-                    padding: '20px',
-                    backgroundColor: '#282c34',
-                    borderRadius: '10px',
-                    color: '#fff',
-                    fontSize: '24px',
-                    fontWeight: 'bold',
-                    textAlign: 'center',
-                    boxShadow: '0px 0px 10px 3px rgba(0, 0, 0, 0.2)',
-                    border: '1px solid #fff'
-                }}>Correct!</div>
-            }
         </div>
+
     );
 }
 
