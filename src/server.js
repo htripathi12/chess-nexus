36d3bfb56082d10af5af427f0ab93d88564d2215
htripathi12 <tripathh@purdue.edu>
2024-06-22 22:08:54 -0400

    begin stockfish integration, finished analyze routing

--- src/server.js
+++ src/server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const cors = require('cors');
 const { router: puzzleRouter, loadPuzzles } = require('./randomPuzzle');
 const db = require('./database');
+const analyze = require('./analyze');
 
 const app = express();
 const PORT = 3000;
@@ -10,6 +11,7 @@ app.use(express.json());
 app.use(cors());
 app.use("/login", db);
 app.use("/puzzles", puzzleRouter);
+app.use("/play", analyze);
 //
 const startServer = async () => {
     try {
