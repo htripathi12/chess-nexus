8621a7bc42d4441cc9a21bfed710dd43b13959a3
htripathi12 <tripathh@purdue.edu>
2024-06-22 23:05:22 -0400

    finished pgn textarea + established async server comms

--- src/pages/Play.js
+++ src/pages/Play.js
@@ -1,5 +1,5 @@
 import React, { useState, useRef, useEffect } from 'react';
-import { Link, Button, Text } from '@chakra-ui/react';
+import { Textarea, Link, Button, Text } from '@chakra-ui/react';
 import { BrowserRouter as Router, Link as RouterLink } from 'react-router-dom';
 import CustomBoard from '../components/CustomBoard';
 import { Chess } from 'chess.js';
@@ -38,7 +38,7 @@ function Play() {
         });
     };
 
-    const loadPGN = async (sourceSquare, targetSquare) => {
+    const handleSubmit = async (sourceSquare, targetSquare) => {
         try {
             const response = await axios.post('http://localhost:3000/play');
             console.log(response.data);
@@ -56,15 +56,15 @@ function Play() {
                         width="27" height="27" viewBox="0 0 24 24" strokeWidth="1.5" stroke="#ffffff" fill="none" 
                         strokeLinecap="round" strokeLinejoin="round">
                         <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
-                        <path d="M9.586 4l-6.586 6.586a2 2 0 0 0 0 2.828l6.586 6.586a2 2 0 0 0 2.18 .434l.145 -.068a2 2 0 0 
+                        <path d="M9.586 4l-6.586 6.586a2 2 0 0 0 0 2.828l6.586 6.586a2 2 0 0 0 2.18 .434l .145 -.068a2 2 0 0 
                             0 1.089 -1.78v-2.586h7a2 2 0 0 0 2 -2v-4l-.005 -.15a2 2 0 0 0 -1.995 -1.85l-7 -.001v-2.585a2 2 0
                             0 0 -3.414 -1.414z" strokeWidth="0" fill="currentColor" />
                     </svg>
                     <Text ml={2}>Back</Text>
                 </Button>
             </Link>
-            <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center', marginTop: '30px'}}>
-                <div>
+            <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'flex-start', marginTop: '30px'}}>
+                <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
                     <CustomBoard
                         ref={customBoardRef}
                         fen={fen}
@@ -72,14 +72,41 @@ function Play() {
                         setWinLoss={setWinLoss}
                         chessInstance={chessInstance.current}
                     />
-                    <div style={{ display: 'flex', justifyContent: 'space-between', width: '100%', marginTop: '3', padding: '3px' }}>
-                        <Button style={{ marginTop: '10px', alignSelf: 'flex-start' }} onClick={handleUndo}>
-                            Previous Move
-                        </Button>
-                        <Button style={{ marginTop: '10px', alignSelf: 'flex-start' }} onClick={loadPGN}>
-                            Load PGN
-                        </Button>
-                    </div>
+                    <Button style={{ marginTop: '10px', alignSelf: 'flex-start' }} onClick={handleUndo}>
+                        Previous Move
+                    </Button>
+                </div>
+                <div style={{ 
+                    display: 'flex', 
+                    flexDirection: 'column', 
+                    alignItems: 'flex-end',
+                    marginLeft: '20px', 
+                }}>
+                    <Textarea
+                        width="300px"
+                        height="300px"
+                        fontSize="16px"
+                        padding="10px"
+                        resize="none"
+                        border="3px solid"
+                        borderColor="#1E8C87"
+                        borderRadius="8px"
+                        placeholder="Enter PGN"
+                        focusBorderColor="#008080"
+                        _hover={{
+                            borderColor: "#008080",
+                        }}
+                        sx={{
+                            '::placeholder': {
+                                color: '#008080',
+                            },
+                        }}
+                    />
+                    <Button style={{ 
+                        marginTop: '10px',
+                    }} onClick={handleSubmit}>
+                        Submit
+                    </Button>
                 </div>
             </div>
             {winLoss && <div>{winLoss}</div>}
