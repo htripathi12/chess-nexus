b920a275752429b0a05f944aaa3c4048decc9f03
htripathi12 <tripathh@purdue.edu>
2024-07-02 13:06:26 -0400

    configured stockfish for returning best move accordingly, adjust depth + stockfish version

--- src/server.js
+++ src/server.js
@@ -7,6 +7,7 @@ const analyze = require('./analyze');
 const app = express();
 const PORT = 3000;
 
+app.use(express.static('public'));
 app.use(express.json());
 app.use(cors());
 app.use("/login", db);
