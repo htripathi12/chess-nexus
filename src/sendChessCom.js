66be7bd63e894b483aa63da6217206d67f844e8b
htripathi12 <tripathh@purdue.edu>
2024-07-22 14:23:02 -0400

    fix database split + start username linking

---
+++ src/sendChessCom.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const mysql = require('mysql');
+const router = express.Router();
+
+
+router.post("/", (req, res) => {
+    const { chesscomUsername } = req.body;
+    console.log(`Received Chess.com username: ${chesscomUsername}`);
+    req.db.query('UPDATE users SET chesscom = ? WHERE id = ?', [chesscomUsername, 1], (err, result) => {
+        if (err) {
+            console.error('Error updating user:', err);
+            return res.status(500).send('Error updating user');
+        }
+    });
+    res.json({ message: 'Received Chess.com username' });
+});
+
+
+module.exports = router;
