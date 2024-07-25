84e2857b3c4f4019cb339ee5dbdae7bf60e77846
htripathi12 <tripathh@purdue.edu>
2024-07-25 00:47:21 -0400

    fixed cookie remembering for usernames

--- src/AuthContext.js
+++ src/AuthContext.js
@@ -21,20 +21,35 @@ export const AuthProvider = ({ children }) => {
         return null;
     };
 
-    const login = (token) => {
-        document.cookie = `token=${token}; path=/`;
+    const login = (token, lichessUser, chesscomUser) => {
+        const expirationDate = new Date();
+        expirationDate.setTime(expirationDate.getTime() + 3 * 60 * 60 * 1000); // 3 hours from now
+        
+        document.cookie = `token=${token}; path=/; expires=${expirationDate.toUTCString()}`;
+        document.cookie = `lichessUsername=${lichessUser || ''}; path=/; expires=${expirationDate.toUTCString()}`;
+        document.cookie = `chesscomUsername=${chesscomUser || ''}; path=/; expires=${expirationDate.toUTCString()}`;
+        
         setIsLoggedIn(true);
+        setLichessUsername(lichessUser || '');
+        setChesscomUsername(chesscomUser || '');
     };
-    
 
     const logout = () => {
         document.cookie = 'token=; path=/; expires=Thu, 01 Jan 1970 00:00:00 GMT';
+        document.cookie = 'lichessUsername=; path=/; expires=Thu, 01 Jan 1970 00:00:00 GMT';
+        document.cookie = 'chesscomUsername=; path=/; expires=Thu, 01 Jan 1970 00:00:00 GMT';
         setIsLoggedIn(false);
+        setLichessUsername('');
+        setChesscomUsername('');
     };
 
     useEffect(() => {
         if (checkTokenCookie()) {
             setIsLoggedIn(true);
+            const lichessUser = document.cookie.split('; ').find(row => row.startsWith('lichessUsername='))?.split('=')[1] || '';
+            const chesscomUser = document.cookie.split('; ').find(row => row.startsWith('chesscomUsername='))?.split('=')[1] || '';
+            setLichessUsername(lichessUser);
+            setChesscomUsername(chesscomUser);
         }
     }, []);
 
