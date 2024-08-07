c6e432292ca81147df1fde3424a0307f069f752a
htripathi12 <tripathh@purdue.edu>
2024-08-07 19:05:20 -0400

    fetching chesscom puzzle rating properly

--- src/routes/randomPuzzle.js
+++ src/routes/randomPuzzle.js
@@ -1,6 +1,7 @@
 const fs = require('fs');
 const express = require('express');
 const csv = require('csv-parser');
+const axios = require('axios');
 const router = express.Router();
 
 let puzzles = [];
@@ -22,12 +23,26 @@ const loadPuzzles = async () => {
     });
 };
 
+const averageRatings = async (chesscomUsername, lichessUsername) => {
+    try {
+        const ccTacticsRating = await axios.get(`https://www.chess.com/callback/member/stats/${chesscomUsername}`);
+        let ccRating = ccTacticsRating.data.stats[2]['stats'].rating;
+
+        const lcTacticsRating = await axios.get(`https://lichess.org/api/user/${lichessUsername}`);
+    } catch (error) {
+        console.error('Error fetching ratings:', error);
+        return { ccRating: 1500, lcRating: 1500 };
+    }
+};
+
 router.get("/averageRating", (req, res) => {
     const { chesscomUsername, lichessUsername } = req.query;
+    console.log('Usernames:', chesscomUsername, lichessUsername);
     if (!chesscomUsername && !lichessUsername) {
         return res.status(400).send({ error: 'User not logged in' });
     }
-    res.send({ rating: 1500 });
+
+    res.send({ rating: averageRatings(chesscomUsername, lichessUsername) });
 });
 
 router.get('/', (req, res) => {
