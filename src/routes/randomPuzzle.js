6358ffcb070673bb701f6dd2fd598a07f93e8842
htripathi12 <tripathh@purdue.edu>
2024-08-07 21:58:27 -0400

    Revert "puzzle rating backend comms"
    This reverts commit 6ef2d2db4783430d08f17c65c31ff480df91f402.

--- src/routes/randomPuzzle.js
+++ src/routes/randomPuzzle.js
@@ -23,28 +23,6 @@ const loadPuzzles = async () => {
     });
 };
 
-const averageRatings = async (chesscomUsername, lichessUsername) => {
-    try {
-        const ccTacticsRating = await axios.get(`https://www.chess.com/callback/member/stats/${chesscomUsername}`);
-        let ccRating = ccTacticsRating.data.stats[2]['stats'].rating;
-
-        const lcTacticsRating = await axios.get(`https://lichess.org/api/user/${lichessUsername}`);
-    } catch (error) {
-        console.error('Error fetching ratings:', error);
-        return { ccRating: 1500, lcRating: 1500 };
-    }
-};
-
-router.get("/averageRating", (req, res) => {
-    const { chesscomUsername, lichessUsername } = req.query;
-    console.log('Usernames:', chesscomUsername, lichessUsername);
-    if (!chesscomUsername && !lichessUsername) {
-        return res.status(400).send({ error: 'User not logged in' });
-    }
-
-    res.send({ rating: averageRatings(chesscomUsername, lichessUsername) });
-});
-
 router.get('/', (req, res) => {
     if (puzzles.length === 0) {
         return res.status(500).send({ error: 'No FENs loaded' });
