89baaa3cf063a3e418ae73fd315d4deb782a9163
htripathi12 <tripathh@purdue.edu>
2024-08-13 16:34:23 -0400

    fix drag and drop issue

--- src/server.js
+++ src/server.js
@@ -83,7 +83,7 @@ app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, '../public', 'index.html'));
 });
 
-const server = app.listen(PORT, () => {
+const server = app.listen(PORT, "0.0.0.0",  () => {
     console.log(`Server listening on port ${PORT}`);
 });
 
