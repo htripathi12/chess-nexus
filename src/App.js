62a09fb56729db9cf36cefbfd28e8d78ce7657ee
htripathi12 <tripathh@purdue.edu>
2024-08-14 00:05:49 -0400

    fix build issue

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React from 'react';
 import { Box, Container, ChakraProvider } from '@chakra-ui/react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import RoutesAndNavbar from './components/RoutesAndNavbar.js';
