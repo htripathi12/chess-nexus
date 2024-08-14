b2d0282e84373f915a9a9ce459f9fae4f1223c60
htripathi12 <tripathh@purdue.edu>
2024-08-13 23:54:55 -0400

    initial landing page

--- src/App.js
+++ src/App.js
@@ -1,10 +1,6 @@
-import React from 'react';
-import {
-  Box, Center, Container, Flex, Button, ChakraProvider, Heading, Stack
-} from '@chakra-ui/react';
-import { Link as RouterLink, BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import { DndProvider } from 'react-dnd';
-import { HTML5Backend } from 'react-dnd-html5-backend';
+import React, { useEffect } from 'react';
+import { Box, Container, ChakraProvider } from '@chakra-ui/react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import RoutesAndNavbar from './components/RoutesAndNavbar.js';
 import { AuthProvider } from './AuthContext.js';
 
@@ -17,51 +13,29 @@ import Signup from './pages/Signup.js';
 import About from './pages/About.js';
 import Contact from './pages/Contact.js';
 import Account from './pages/Account.js';
+import LandingPage from './pages/LandingPage.js';
 
 function App() {
   return (
     <AuthProvider>
       <Router>
         <ChakraProvider>
-          <DndProvider backend={HTML5Backend}>
-            <Box bg="teal.400" minHeight="100vh" minWidth="100vw" overflow="hidden">
-              <RoutesAndNavbar />
-              <Container maxW="100%">
-                <Routes>
-                  <Route path="/" element={
-                    <Center>
-                      <Flex direction="column" align="center" mt="10">
-                        <Stack spacing={6} textAlign="center" align="center" maxW="lg">
-                          <Heading as="h1" size="2xl" color="white">
-                            Welcome to Chess Nexus
-                          </Heading>
-                        </Stack>
-                        <Stack spacing={4} direction="row" align="center">
-                          <Button as={RouterLink} to="/play" size="lg" colorScheme="teal">
-                            Play
-                          </Button>
-                          <Button as={RouterLink} to="/puzzles" size="lg" colorScheme="teal">
-                            Puzzles
-                          </Button>
-                          <Button as={RouterLink} to="/learn" size="lg" colorScheme="teal">
-                            Learn
-                          </Button>
-                        </Stack>
-                      </Flex>
-                    </Center>
-                  }/>
-                  <Route path="/play" element={<Play />} />
-                  <Route path="/puzzles" element={<Puzzles />} />
-                  <Route path="/learn" element={<Learn />} />
-                  <Route path="/login" element={<Login />} />
-                  <Route path="/signup" element={<Signup />} />
-                  <Route path="/about" element={<About />} />
-                  <Route path="/contact" element={<Contact />} />
-                  <Route path="/account" element={<Account />} />
-                </Routes>
-              </Container>
-            </Box>
-          </DndProvider>
+          <Box bg="teal.400" minHeight="100vh" minWidth="100vw" overflow="hidden">
+            <RoutesAndNavbar />
+            <Container maxW="100%">
+              <Routes>
+                <Route path="/" element={<LandingPage />} />
+                <Route path="/play" element={<Play />} />
+                <Route path="/puzzles" element={<Puzzles />} />
+                <Route path="/learn" element={<Learn />} />
+                <Route path="/login" element={<Login />} />
+                <Route path="/signup" element={<Signup />} />
+                <Route path="/about" element={<About />} />
+                <Route path="/contact" element={<Contact />} />
+                <Route path="/account" element={<Account />} />
+              </Routes>
+            </Container>
+          </Box>
         </ChakraProvider>
       </Router>
     </AuthProvider>
