03d422e313144dee11362cee41b01f9f9ca79b52
htripathi12 <tripathh@purdue.edu>
2024-08-15 18:19:42 -0400

    test again

--- src/server/routes/randomPuzzle.js
+++ src/server/routes/randomPuzzle.js
@@ -7,7 +7,7 @@ let puzzles = [];
 
 const loadPuzzles = async () => {
     return new Promise((resolve, reject) => {
-        const filePath = '/var/lib/containers/railwayapp/bind-mounts/cb3f947e-9986-47dd-b703-984f515559a4/vol_lb4qzc90ynxb6v68';
+        const filePath = '/var/lib/containers/railwayapp/bind-mounts/21b069ba-a55b-44d8-9d92-2f0388f1c5bf/vol_lb4qzc90ynxb6v68/lichess_db_puzzle.csv';
         console.log(`Attempting to read file at: ${filePath}`);
 
         fs.createReadStream(filePath)
