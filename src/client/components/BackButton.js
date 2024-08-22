c0c62fe1bead8df5accd39bd96b2ce6cf847bbcd
htripathi12 <tripathh@purdue.edu>
2024-08-22 16:35:08 -0400

    mess with svgs a bit

--- src/client/components/BackButton.js
+++ src/client/components/BackButton.js
@@ -5,16 +5,16 @@ import { Button, Text, Link } from '@chakra-ui/react';
 function BackButton() {
     return (
         <Link as={RouterLink} to="/" _hover={{ textDecoration: "none" }}>
-            <Button marginTop="3" bg='teal.400' border="1px" color="white" _hover={{ bg: "teal.700", color: "white" }} width="auto">
+            <Button marginTop="3" width="auto">
                 <svg xmlns="http://www.w3.org/2000/svg" className="icon icon-tabler icon-tabler-arrow-big-left-filled" 
-                    width="27" height="27" viewBox="0 0 24 24" strokeWidth="1.5" stroke="#ffffff" fill="none" 
+                    width="27" height="27" viewBox="0 0 24 24" strokeWidth="1.5" stroke="black" fill="white" 
                     strokeLinecap="round" strokeLinejoin="round">
                     <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
                     <path d="M9.586 4l-6.586 6.586a2 2 0 0 0 0 2.828l6.586 6.586a2 2 0 0 0 2.18 .434l .145 -.068a2 2 0 0 
                         0 1.089 -1.78v-2.586h7a2 2 0 0 0 2 -2v-4l-.005 -.15a2 2 0 0 0 -1.995 -1.85l-7 -.001v-2.585a2 2 0
-                        0 0 -3.414 -1.414z" strokeWidth="0" fill="currentColor" />
+                        0 0 -3.414 -1.414z" strokeWidth="0" fill="#008080" />
                 </svg>
-                <Text ml={2}>Back</Text>
+                <Text ml={2} color="#008080">Back</Text>
             </Button>
         </Link>
     );
