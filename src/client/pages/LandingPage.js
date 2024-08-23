e72f2d1008aad122b5bba9527860829e1621da65
htripathi12 <tripathh@purdue.edu>
2024-08-22 23:41:22 -0400

    remembering daily puzzle properly

--- src/client/pages/LandingPage.js
+++ src/client/pages/LandingPage.js
@@ -1,9 +1,10 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import { Link as RouterLink } from 'react-router-dom';
 import { Flex, Stack, Box, Text } from '@chakra-ui/react';
 import { motion } from 'framer-motion';
 import axios from 'axios';
 import CustomBoard from '../components/CustomBoard';
+import { Chess } from 'chess.js';
 
 const GradientButton = ({ to, children }) => (
   <Box
@@ -18,8 +19,8 @@ const GradientButton = ({ to, children }) => (
     mb={4}
   >
     <motion.div
-    initial={{ scale: 1, background: 'linear-gradient(90deg, #81E6D9, #319795)' }}
-    whileHover={{ scale: 1.05, background: 'linear-gradient(90deg, #319795, #81E6D9)' }}
+      initial={{ scale: 1, background: 'linear-gradient(90deg, #81E6D9, #319795)' }}
+      whileHover={{ scale: 1.05, background: 'linear-gradient(90deg, #319795, #81E6D9)' }}
       transition={{ duration: 0.3 }}
       style={{
         width: '100%',
@@ -38,6 +39,10 @@ const GradientButton = ({ to, children }) => (
 );
 
 export default function LandingPage() {
+  const [fen, setFen] = useState(
+    localStorage.getItem('lastPuzzleFEN') || "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1"
+  );
+  const chess = useRef(new Chess());
 
   useEffect(() => {
     document.body.style.overflow = 'hidden';
@@ -45,27 +50,30 @@ export default function LandingPage() {
       document.body.style.overflow = 'auto';
     };
   }, []);
-  
-    // Fetch daily puzzle on component mount
-    useEffect(() => {
-      const fetchPuzzle = async () => {
-        try {
-          const response = await axios.get(process.env.PUBLIC_URL + '/puzzle');
-          const puzzle = response.data.puzzle;
-          console.log(puzzle);
-        } catch (error) {
-          console.error(error);
+
+  useEffect(() => {
+    const fetchPuzzle = async () => {
+      try {
+        if (!localStorage.getItem('lastPuzzleFEN')) {
+          const response = await axios.get('http://localhost:8080/puzzle');
+          console.log(response.data.puzzle.body.fen);
+          chess.current.load(response.data.puzzle.body.fen);
+          const currentFEN = chess.current.fen();
+          setFen(currentFEN);
+          localStorage.setItem('lastPuzzleFEN', currentFEN);
+        } else {
+          chess.current.load(fen);
         }
+      } catch (error) {
+        console.error(error);
+      }
     };
     fetchPuzzle();
-  }, []);
+  }, [fen]);
 
   return (
-      <Flex align="center" mt="10" width="100%" justifyContent="center">
-          <CustomBoard
-              disableBoard={true}
-          
-          />
+    <Flex align="center" mt="10" width="100%" justifyContent="center">
+      {fen && <CustomBoard fen={fen} chessInstance={chess.current} />}
       <Stack spacing={4} direction="column" align="center" pl="150px">
         <GradientButton to="/play">Play ♟️</GradientButton>
         <GradientButton to="/puzzles">Puzzles 🧩</GradientButton>
