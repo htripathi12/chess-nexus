e4171662d16eed80e99ccad3dd509b8fae1d91ca
htripathi12 <tripathh@purdue.edu>
2025-02-07 18:05:14 -0500

    testing sharedarraybuffer fix

--- src/server.js
+++ src/server.js
@@ -16,6 +16,13 @@ const dailyPuzzle = require('./server/dailyPuzzle');
 
 const app = express();
 
+// 1. Add cross-origin isolation headers
+app.use((req, res, next) => {
+  res.setHeader("Cross-Origin-Opener-Policy", "same-origin");
+  res.setHeader("Cross-Origin-Embedder-Policy", "require-corp");
+  next();
+});
+
 const PORT = process.env.PORT || 8080;
 console.log('PORT:', PORT);
 
